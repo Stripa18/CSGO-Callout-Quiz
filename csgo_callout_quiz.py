a1b009f59ca72c9df3b651c19fb47c1f424548d6
Stripa18 <82184098+Stripa18@users.noreply.github.com>
2021-05-04 15:50:50 +0200

    More questions, and 100+ lines of code milestone!
    I added two more questions with their answers, also I have officially coded more than 100 lines! It's awesome that I am keeping up with this project. I recently broke my foot and I have hard times walking, so I'll sit more and code the quiz meanwhile. Also, remember! Tomorrow I'll get the ranks.

--- csgo_callout_quiz.py
+++ csgo_callout_quiz.py
@@ -4,6 +4,7 @@
 # The videogame is a first-person shooter which has different maps, all of which has strategical spots players can be positioned in.
 # Callouts are used to define these spots and to *call out* an enemy's position so that the team can attack and defeat that player.
 # In the quiz, a spot will be described, and the user must enter the name of the callout in lowercase.
+# Callouts from https://totalcsgo.com/callouts/
 import time
 score = 0
 show_tip_end = False
@@ -15,6 +16,9 @@ answer5 = ['jungle', 'Jungle', 'JUNGLE']
 answer6 = ['sneaky', 'Sneaky', 'SNEAKY']
 yes = ['yes', 'Yes', 'YES', 'ye', 'Ye', 'YE', 'Y', 'y', 'sure', 'Sure', 'SURE', 'Yep', 'yep', 'YEP']
 no = ['no', 'No', 'NO', 'nop', 'Nop', 'NOP', 'nope', 'Nope', 'NOPE', 'nah', 'Nah', 'NAH', 'N', 'n']
+answer7 = ['tetris', 'Tetris', 'TETRIS']
+answer8 = ['triple box', 'triple_box', 'Triple box', 'Triple_box', 'TRIPLE_BOX', 'TRIPLE BOX', 'triple', 'Triple', 'TRIPLE', 'triple stack', 'triple_stack', 'Triple stack', 'Triple Stack', 'Triple_stack', 'Triple_Stack', 'TRIPLE STACK', 'TRIPLE_STACK']
+answer9 = []
 print("Welcome to the Callouts Quiz!")
 time.sleep(1)
 print("All answers must be entered in lowercase.")
@@ -90,10 +94,44 @@ if input() in yes:
 if input() in no:
     print("Sure! Let's get on with the quiz then.")
 time.sleep (3)
+print("This is an easy and common one.")
+time.sleep(2)
+print("In front of A Ramp, there is a stack of boxes and square stone clusters which people can hide behind.")
+time.sleep(1)
+print("What's the callout called like?")
+if input() in answer7:
+    print("Correct.")
+    score += 1
+else:
+    print("Incorrect! Bad, bad...")
+    score -= 1
+time.sleep(2)
+print("This time, I will make It hard! I will not tell you where the callout is, rather how It looks.")
+time.sleep(1)
+print("Get ready.")
+time.sleep(3)
+print("A triple set of boxes players can hide behind and peek to enemies coming.")
+if input() in answer8:
+    print("I see you know the common callouts for people to be in well, that's good.")
+    score += 1
+else:
+    print("Wrong. This is a common place for people to be in, It's in A. Come on. Let's continue.")
+    score -= 1
+time.sleep(2)
+print("This is the hardest question! If you get this one right you will obtain triple the points!")
+time.sleep(3)
+print("An small alcove between two stacks of boxes, that is often left unchecked by Terorrists.")
+if input() in answer9:
+    time.sleep(1)
+    print("Holy moly! You got It right...? I can't believe It! Wonderful, wonderful. You must have a good knowledge of the callouts. You have obtained triple the points. Let's continue.")
+    score += 3
+else:
+    print("Oh, that's okay, don't worry about It. It was very hard to get It right unless you knew the bombsite.")
+    score -= 1
 # End of the quiz, there WILL be more questions and answers above though, this is just for the future.
 time.sleep(1)
 print("Good job on doing the quiz! Here is your final score...")
 time.sleep(3)
 print(score)
 if score <= 0:
-    print("Yikes, you didn't do very well... You don't have a rank yet. Try again and practice more!")
+   print("Yikes, you didn't do very well... You don't have a rank yet. Try again and practice more!")
